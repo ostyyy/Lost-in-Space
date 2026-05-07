6ac4053a1e69ad0f1c9729c88fbf4fe881974569
ostyyy <ggg468432@gmail.com>
2026-05-07 21:01:03 +0300

    Add Post model

--- SpaceServer/Models/Post.cs
+++ SpaceServer/Models/Post.cs
@@ -2,5 +2,15 @@
 {
     public class Post
     {
+        public int ID { get; set; }
+        public string Content { get; set; } = string.Empty;
+
+        public int AuthorID { get; set; }
+        public User? Author { get; set; }
+
+        public int TopicID { get; set; }
+        public Topic? Topic { get; set; }
+
+        public DateTime CreatedDate { get; set; }
     }
 }
