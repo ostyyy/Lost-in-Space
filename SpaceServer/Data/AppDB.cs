9714cce4781f4ced13597e23662c4a0cfc988ec0
ostyyy <ggg468432@gmail.com>
2026-05-07 21:19:56 +0300

    Implement AppDB context

--- SpaceServer/Data/AppDB.cs
+++ SpaceServer/Data/AppDB.cs
@@ -1,6 +1,17 @@
-﻿namespace SpaceServer.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceServer.Models;
+
+namespace SpaceServer.Data
 {
-    public class AppDB
+    public class AppDB : DbContext
     {
+        public AppDB(DbContextOptions<AppDB> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Launches> Launches { get; set; }
+        public DbSet<ImageOfTheDay> ImagesOfTheDay { get; set; }
     }
 }
