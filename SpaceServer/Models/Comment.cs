06a1d0b33ee4e0196000e01f324f35c5171282c7
ostyyy <ggg468432@gmail.com>
2026-05-07 21:03:23 +0300

    Add Comment model

--- SpaceServer/Models/Comment.cs
+++ SpaceServer/Models/Comment.cs
@@ -2,5 +2,20 @@
 {
     public class Comment
     {
+        public int ID { get; set; }
+        public string Content { get; set; } = string.Empty;
+
+        public int? AuthorID { get; set; }
+        public User? Author { get; set; }
+
+        public int PostID { get; set; }
+        public Post? Post { get; set; }
+
+        public int? ParentCommentID { get; set; }
+        public Comment? ParentComment { get; set; }
+
+        public List<Comment> Replies { get; set; } = new();
+
+        public DateTime CreatedDate { get; set; }
     }
 }
