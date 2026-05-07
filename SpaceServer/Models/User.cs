78d920328dce426a6857a6fc2539a984ecdf4a0d
beliyugnetatel <reddmk2@gmail.com>
2026-05-07 21:21:12 +0300

    Update User.cs

--- SpaceServer/Models/User.cs
+++ SpaceServer/Models/User.cs
@@ -3,8 +3,8 @@
     public class User
     {
         public int Id { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
 
         public List<Topic> Topics { get; set; } = new List<Topic>();
         public List<Post> Posts { get; set; } = new List<Post>();
