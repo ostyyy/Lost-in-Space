c3a375c5dc741d2298663212c827be89fd3acf1b
ostyyy <ggg468432@gmail.com>
2026-05-07 20:58:27 +0300

    Add Topic model

--- SpaceServer/Models/Topic.cs
+++ SpaceServer/Models/Topic.cs
@@ -2,5 +2,11 @@
 {
     public class Topic
     {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int AuthorID { get; set; }
+        public User? Author { get; set; }
+
+        public List<Post> Posts { get; set; } = new();
     }
 }
