d4d5480b0039cd59f4affa68af747b925cf3a11f
ostyyy <ggg468432@gmail.com>
2026-05-12 16:58:40 +0300

    Update LaunchesController.cs

--- SpaceServer/Controllers/LaunchesController.cs
+++ SpaceServer/Controllers/LaunchesController.cs
@@ -34,5 +34,51 @@ namespace SpaceServer.Controllers
 
             return launch;
         }
+
+        // POST: api/Launches
+        [HttpPost]
+        public async Task<ActionResult<Launches>> PostLaunch(Launches launch)
+        {
+            _context.Launches.Add(launch);
+            await _context.SaveChangesAsync();
+
+            // Повертає статус 201 Created і посилання на новий об'єкт
+            return CreatedAtAction(nameof(GetLaunch), new { id = launch.Id }, launch);
+        }
+
+        // PUT: api/Launches/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLaunch(int id, Launches launch)
+        {
+            if (id != launch.Id) return BadRequest();
+
+            _context.Entry(launch).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Launches.Any(e => e.Id == id)) return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Launches/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLaunch(int id)
+        {
+            var launch = await _context.Launches.FindAsync(id);
+            if (launch == null) return NotFound();
+
+            _context.Launches.Remove(launch);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
