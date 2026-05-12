344c90b586bde26b49f1701b4433d8871ef335c0
ostyyy <ggg468432@gmail.com>
2026-05-12 16:00:30 +0300

    fix Comment model

--- SpaceServer/Models/Comment.cs
+++ SpaceServer/Models/Comment.cs
@@ -1,21 +1,31 @@
-﻿namespace SpaceServer.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpaceServer.Models
 {
+    [Table("comments")] 
     public class Comment
     {
+        [Column("id")] 
         public int ID { get; set; }
+
+        [Column("content")]
         public string Content { get; set; } = string.Empty;
 
+        [Column("authorid")]
         public int? AuthorID { get; set; }
         public User? Author { get; set; }
 
+        [Column("postid")]
         public int PostID { get; set; }
         public Post? Post { get; set; }
 
+        [Column("parentcommentid")] 
         public int? ParentCommentID { get; set; }
         public Comment? ParentComment { get; set; }
-
+      
         public List<Comment> Replies { get; set; } = new();
 
+        [Column("createddate")] 
         public DateTime CreatedDate { get; set; }
     }
 }
