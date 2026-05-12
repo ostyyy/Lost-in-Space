e46ab514b5171dc1122d9c14d8266794b536db09
ostyyy <ggg468432@gmail.com>
2026-05-12 15:35:00 +0300

    fix Topic and Launches models

--- SpaceServer/Models/Launches.cs
+++ SpaceServer/Models/Launches.cs
@@ -1,12 +1,26 @@
-﻿namespace SpaceServer.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpaceServer.Models
 {
+    [Table("launches")] 
     public class Launches
     {
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("launchname")] 
         public string LaunchName { get; set; } = string.Empty;
-        public DateTime LaunchDate { get; set; }
+
+        [Column("launchdate")]
+        public DateTime? LaunchDate { get; set; }
+
+        [Column("launchprovider")]
         public string LaunchProvider { get; set; } = string.Empty;
+
+        [Column("status")]
         public string Status { get; set; } = string.Empty;
+
+        [Column("launchlocation")]
         public string LaunchLocation { get; set; } = string.Empty;
     }
 }
--- SpaceServer/Models/Topic.cs
+++ SpaceServer/Models/Topic.cs
@@ -1,10 +1,19 @@
-﻿namespace SpaceServer.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpaceServer.Models
 {
+    [Table("topics")] 
     public class Topic
     {
+        [Column("id")] 
         public int ID { get; set; }
+
+        [Column("title")]
         public string Title { get; set; } = string.Empty;
+
+        [Column("authorid")]
         public int AuthorID { get; set; }
+
         public User? Author { get; set; }
 
         public List<Post> Posts { get; set; } = new();
