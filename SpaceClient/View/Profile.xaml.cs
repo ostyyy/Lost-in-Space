47412ebb2824d93392c88a6094c0ca6b67eda21a
beliyugnetatel <reddmk2@gmail.com>
2026-05-23 21:19:08 +0300

    Update profile menu

--- SpaceClient/View/Profile.xaml.cs
+++ SpaceClient/View/Profile.xaml.cs
@@ -23,5 +23,10 @@ namespace SpaceClient.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
