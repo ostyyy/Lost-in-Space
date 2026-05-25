cce6d740203bf2f6a8cf49b305a5f90f27b4576e
ostyyy <ggg468432@gmail.com>
2026-05-25 23:21:51 +0300

    register: mask psw

--- SpaceClient/View/RegisterWindow.xaml.cs
+++ SpaceClient/View/RegisterWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpaceClient.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,22 @@ namespace SpaceClient.View
         public RegisterWindow()
         {
             InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is RegisterViewModel viewModel)
+            {
+                viewModel.Password = ((PasswordBox)sender).Password;
+            }
+        }
+
+        private void ConfirmPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is RegisterViewModel viewModel)
+            {
+                viewModel.ConfirmPassword = ((PasswordBox)sender).Password;
+            }
         }
     }
 }
